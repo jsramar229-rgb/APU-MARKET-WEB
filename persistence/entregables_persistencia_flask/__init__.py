30bed69279110f6537defbf2ffa12e6d908b1ee2
jsramar229-rgb <jsramar.229@gmail.com>
2026-09-22 00:08:26 -0500

    Delete persistence directory

--- persistence/entregables_persistencia_flask/__init__.py
+++
@@ -1,4 +0,0 @@
-from .extensions import db, migrate
-from .models import Address, Customer, Order, OrderEvent, OrderItem
-
-__all__ = ["db", "migrate", "Address", "Customer", "Order", "OrderEvent", "OrderItem"]
